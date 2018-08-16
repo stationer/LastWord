914802adc48a757b7c3d3bfd6df9fc57318ff6c0
Tw Dover <tw.dover@bettercarpeople.com>
2018-08-16 17:44:44 -0400

    bootstrap: bootstraped lastword templates

--- src/templates/LW.Edit.php
+++ src/templates/LW.Edit.php
@@ -1,73 +1,132 @@
 <?php echo $this->render('header'); ?>
-        <div class="lastWord">
-            <div class="form">
-                <div class="top"><h3>Edit An Account</h3></div>
-            <form action="" method="post" id="lw_editForm">
-                <table>
-                    <tr>
-                        <th>Record Id</th>
-                        <td><?php echo $account->lwr_id; ?><input type="hidden" name="lwr_id" value="<?php echo $account->lwr_id; ?>"></td>
-                    </tr>
-                    <tr>
-                        <th>Delete?</th>
-                        <td><input type="checkbox" name="delete" value="<?php echo $account->lwr_id; ?>"></td>
-                        <td class="red">Warning:  Deleting cannot be undone.</td>
-                    </tr>
-                    <tr>
-                        <th>Service Name</th>
-                        <td><input type="text" size="20" name="service" value="<?php html($account->service); ?>"></td>
-                        <td class="red">Warning:  Changing This will result in a different password!</td>
-                    </tr>
-                    <tr>
-                        <th>User Name</th>
-                        <td><input type="text" size="20" name="username" value="<?php html($account->username); ?>"></td>
-                        <td class="red">Warning:  Changing This will result in a different password!</td>
-                    </tr>
-                    <tr>
-                        <th>Password&nbsp;#</th>
-                        <td><input type="text" size="20" name="resetCount" value="<?php html($account->resetCount); ?>"></td>
-                        <td class="red">Warning:  Changing This will result in a different password!</td>
-                    </tr>
-                    <tr>
-                        <th>Password&nbsp;Length</th>
-                        <td><input type="text" size="20" name="passLen" value="<?php html($account->passLen); ?>"></td>
-                        <td class="red">Warning:  Changing This will result in a different password!</td>
-                    </tr>
-                    <tr>
-                        <td colspan="3"><hr></td>
-                    </tr>
-                    <tr>
-                        <th>Presets</th>
-                        <td><select onchange="setLoginDetails(this.value);" title="Optional">
-                                <option value="">Blank/Unknown</option>
-<?php foreach ($Websites as $k => $v) { ?>
-                                <option value="<?php echo $k; ?>"><?php echo $v['label']; ?></option>
-<?php } ?>
-                            </select>
-                        </td>
-                    <tr>
-                        <th>Login&nbsp;URL</th>
-                        <td><input type="text" id="loginURI" size="20" name="loginURI" value="<?php html($account->loginURI); ?>"></td>
-                        <td class="green">Safe to change: set to the address of the Service's login <i>handler</i>.</td>
-                    </tr>
-                    <tr>
-                        <th>Username&nbsp;Field</th>
-                        <td><input type="text" id="userField" size="20" name="userField" value="<?php html($account->userField); ?>"></td>
-                        <td class="green">Advanced: set this to the form fieldname the service expects a username in</td>
-                    </tr>
-                    <tr>
-                        <th>Password&nbsp;Field</th>
-                        <td><input type="text" id="passField" size="20" name="passField" value="<?php html($account->passField); ?>"></td>
-                        <td class="green">Advanced: set this to the form fieldname the service expects a password in</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><input type="submit" value="Submit"></td>
-                    </tr>
-                </table>
-            </form>
-                <div class="bottom"></div></div>
+    <div class="container-fluid">
+        <div class="form">
+            <div class="container col-xl-8 offset-2">
+                <form action="" method="post" id="lw_editForm">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3> Edit An Account</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="lwr_id" class="col-sm-2 col-form-label">Record ID</label>
+                                <label class="col-sm-2 col-form-label"> <?php echo $account->lwr_id; ?></label>
+
+                                <div class="col-sm-3">
+                                    <input type="hidden" class="form-control" id="lwr_id" name="lwr_id" value="<?php echo $account->lwr_id; ?>"
+                                           placeholder="<?php echo $account->lwr_id; ?>">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="delete" class="col-sm-2">Delete?</label>
+                                <div class="col-sm-3">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="delete" name="delete" value="<?php echo $account->lwr_id; ?>">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <label class="form-check-label text-warning" for="delete">
+                                        Warning: Deleting cannot be undone.
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="service" class="col-sm-2 col-form-label">Service Name</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="service" name="service"
+                                           placeholder="<?php html($account->service); ?>" value="<?php html($account->service); ?>">
+                                </div>
+                                <div class="col-sm-6 text-warning">
+                                    Warning: Changing This will result in a different password!
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="username" class="col-sm-2 col-form-label">Username</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control " id="username" name="username"
+                                           placeholder="<?php html($account->username); ?>" value="<?php html($account->username); ?>">
+                                </div>
+                                <div class="col-sm-6 text-warning">
+                                    Warning: Changing This will result in a different password!
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="resetCount" class="col-sm-2 col-form-label">Password #</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="resetCount" name="resetCount"
+                                           value="<?php html($account->resetCount); ?>">
+                                </div>
+                                <div class="col-sm-6 text-warning">
+                                    Warning: Changing This will result in a different password!
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="passLen" class="col-sm-2 col-form-label">Password Length</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="passLen" name="passLen"
+                                           value="<?php html($account->passLen); ?>"
+                                           placeholder="<?php html($account->passLen); ?>">
+                                </div>
+                                <div class="col-sm-6 text-warning">
+                                    Warning: Changing This will result in a different password!
+                                </div>
+                            </div>
+                            <div class="border-top my-3"></div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Preset</label>
+                                <div class="col-sm-3">
+                                    <select class="custom-select" onchange="setLoginDetails(this.value);"
+                                            title="Optional">
+                                        <option value="">Blank/Unknown</option>
+                                        <?php foreach ($Websites as $k => $v) { ?>
+                                            <option value="<?php echo $k; ?>"><?php echo $v['label']; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="loginURI" class="col-sm-2 col-form-label">Login URL</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="loginURI" name="loginURI"
+                                           placeholder="<?php html($account->loginURI); ?>" value="<?php html($account->loginURI); ?>">
+                                </div>
+                                <div class="col-sm-6 text-success">
+                                    Safe to change: set to the address of the Service's login
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="userField" class="col-sm-2 col-form-label">Username Field</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="userField" name="userField"
+                                           value="<?php html($account->userField); ?>"
+                                           placeholder="<?php html($account->userField); ?>">
+                                </div>
+                                <div class="col-sm-6 text-info">
+                                    Advanced: set this to the form fieldname the service expects a username in
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="passField" class="col-sm-2 col-form-label">Password Field</label>
+                                <div class="col-sm-3">
+                                    <input type="text" class="form-control" id="passField" name="passField"
+                                           value="<?php html($account->passField); ?>"
+                                           placeholder="<?php html($account->passField); ?>">
+                                </div>
+                                <div class="col-sm-6 text-info">
+                                    Advanced: set this to the form fieldname the service expects a password in
+                                </div>
+                            </div>
+                            <div>
+                                <input type="submit" class="btn btn-primary" value="Submit">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="bottom"></div>
+            </div>
         </div>
-        <script type="text/javascript"><!--
-            var aLoginDetails = <?php echo json_encode($Websites); ?>;
-            // --></script>
+    </div>
+    <script type="text/javascript"><!--
+        var aLoginDetails = <?php echo json_encode($Websites); ?>;
+        // --></script>
 <?php echo $this->render('footer');
--- src/templates/LW.List.php
+++ src/templates/LW.List.php
@@ -1,56 +1,58 @@
 <?php echo $this->render('header'); ?>
-        <div class="lastWord">
-            <div class="form">
-                <div class="top">
-                    <a style="float:right;" href="/LastWord/add">Add Service</a>
-                    <h3>Master Key</h3>
-                </div>
-                <form action="" method="post" onsubmit="return ajas.event.cancelBubble(event) && false;"><div>
+    <div class="container-fluid">
+        <div class="form">
+            <div>
+                <a style="float:right;" href="/LastWord/add">Add Service</a>
+                <h3>Master Key</h3>
+            </div>
+            <form action="" method="post" onsubmit="return ajas.event.cancelBubble(event) && false;"><div>
                     <label for="lastword_masterKey">Enter your Master Key here:</label>
                     <input type="password" name="" id="lastword_masterKey" onkeydown="return ajas.event.catchReturn(event);"
-                        onkeyup="update_();" onchange="update_();">
-                    <span class="help" id="lastword_keyVisual" title="These icons help you know if you entered your Master Key correctly."><img src="/vendor/stationer/lastword/src/images/EnterKey.gif" alt="Enter your Master Key to get your Passwords" title="Enter your Master Key to get your Passwords"></span>
+                           onkeyup="update_();" onchange="update_();">
+                    <span class="help" id="lastword_keyVisual" title="These icons help you know if you entered your Master Key correctly."><img src="/^LastWord/images/EnterKey.gif" alt="Enter your Master Key to get your Passwords" title="Enter your Master Key to get your Passwords"></span>
                 </div></form>
-                <div class="bottom"></div></div>
-            
-            <table id="lastword_list">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Resource</th>
-                        <th>Username</th>
-                        <th colspan="3" class="resetCount">Password&nbsp;#</th>
-                        <th>Password</th>
-                    </tr>
-                </thead>
-                <tbody id="lastword_passwordList">
+            <div class="bottom"></div></div>
+
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+                <th></th>
+                <th scope="col">Resource</th>
+                <th scope="col">Username</th>
+                <th colspan="3" class="resetCount">Password&nbsp;#</th>
+                <th scope="col">Password</th>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody id="lastword_passwordList">
             <?php
-                if (is_array($list) && count($list) > 0)
+            if (is_array($list) && count($list) > 0)
                 foreach ($list as $k => $v){
                     echo '
                     <tr onmouseover="this.id=\'highlight\';" onmouseout="this.id=\'\';">
                         <td><a href="/LastWord/edit/'.$v->lwr_id.'">edit</a></td>
                         <td id="lw'.$k.'_s">'.$v->service.'</td>
                         <td id="lw'.$k.'_u">'.$v->username.'</td>
-                        <td class="resetCount"><a href="javascript:void(0);" onclick="resetPass('.$k.',-1);return false;"><img width="16" height="16" alt="Choose Previous Password" title="Choose Previous Password" src="/vendor/stationer/lastword/src/images/down.png"></a></td>
+                        <td class="resetCount"><a href="javascript:void(0);" onclick="resetPass('.$k.',-1);return false;"><img width="16" height="16" alt="Choose Previous Password" title="Choose Previous Password" src="/assets/svgs/solid/arrow-down.svg"></a></td>
                         <td id="lw'.$k.'_r" class="resetCount">'.$v->resetCount.'</td>
-                        <td class="resetCount"><a href="javascript:void(0);" onclick="resetPass('.$k.',1);return false;"><img width="16" height="16" alt="Choose Next Password" title="Choose Next Password" src="/vendor/stationer/lastword/src/images/up.png"></a></td>
+                        <td class="resetCount"><a href="javascript:void(0);" onclick="resetPass('.$k.',1);return false;"><img width="16" height="16" alt="Choose Next Password" title="Choose Next Password" src="/assets/svgs/solid/arrow-up.svg"></a></td>
                         <td id="lw'.$k.'_p3" class="checker2" onmouseover="this.className=\'\';" onmouseout="this.className=\'checker2\';">Enter Key</td>
-                        <td>'.(strlen($v->loginURI)<8?'':'<a href="'.$v->loginURI.'" onclick="return remoteLogin('.$k.',document.getElementById(\'lw'.$k.'_p3\').innerHTML);"><img width="16" height="16" alt="login" title="Login Remotely with the Secure password" src="/vendor/stationer/lastword/src/images/right.png"></a>').'</td>
+                        <td>'.(strlen($v->loginURI)<8?'':'<a href="'.$v->loginURI.'" onclick="return remoteLogin('.$k.',document.getElementById(\'lw'.$k.'_p3\').innerHTML);"><img width="16" height="16" alt="login" title="Login Remotely with the Secure password" src="/assets/svgs/solid/arrow-right.svg"></a>').'</td>
                         <td id="lw'.$k.'_m"></td>
                     </tr>';
                 } else {
-                    echo '<tr><td colspan="9">You will need to add a Service above.</td></tr>';
-                }
+                echo '<tr><td colspan="9">You will need to add a Service above.</td></tr>';
+            }
             ?>
-                </tbody>
-            </table>
-            <form id="lwlogin" method="post" action=""><p>
+            </tbody>
+        </table>
+        <form id="lwlogin" method="post" action=""><p>
                 <input type="hidden" name="username" id="lwloginUser">
                 <input type="hidden" name="password" id="lwloginPass">
             </p></form>
-        </div>
-        <script type="text/javascript"><!--
-            aList=<?php echo $json; ?>;
-            // --></script>
+    </div>
+    <script type="text/javascript"><!--
+        aList=<?php echo $json; ?>;
+        // --></script>
 <?php echo $this->render('footer');
